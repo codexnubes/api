f524a6f5ff228bc36986dffa77bd5ec015c4d2eb
root <root@localhost.localdomain>
2017-03-20 09:56:15 +0530

    added completed api with 6 endpoints

--- ec2-api/app.py
+++ ec2-api/app.py
@@ -1,109 +1,143 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask import Flask
+from flask_restful import reqparse
+from flask import request
+from flask import jsonify
+from flask import make_response
 from flask_httpauth import HTTPBasicAuth
+from pymongo import MongoClient
+from bson import json_util
+import pprint
+import flask
+'''
+/api/v1.0/get_data/all
+/api/v1.0/get_region/<region>
+/api/v1.0/get_family/<family>
+/api/v1.0/get_os/<os>
+/api/v1.0/query?date=2017-02-10&region=us-east-1&type=m3.medium
+/api/v1.0/query?date=2017-02-10
+/api/v1.0/query?date=2017-02-10&region=us-east-1
+'''
 auth = HTTPBasicAuth()
+
+client = MongoClient('mongodb://ds153179.mlab.com:53179/')
+db = client['ec2-api']
+db.authenticate('test','123456')
+collection = db['data']
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
-
 @auth.get_password
 def get_username(username):
-    if username == 'chathuranga':
-        return '123456'
+    if username == 'admin':
+        return 'admin'
     return None
 
 @auth.error_handler
 def unauthorized():
     return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
-def make_public_task(task):
-    new_task = {}
-    for field in task:
-        if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
-        else:
-            new_task[field] = task[field]
-    return new_task
+@app.route('/')
+def index():
+	return flask.render_template('index.html')
 
-@app.route('/ec2-api/api/v1.0/tasks', methods=['GET'])
+@app.route('/api/v1.0/get_data/all', methods=['GET'])
 @auth.login_required
-def get_tasks():
-    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+def get_data():
+   data_array=[]
 
-@app.route('/ec2-api/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-@auth.login_required
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
+   for z in collection.find():
+        for x in z['regions'][0]['instanceTypes']:
+            data_array.append({'region':z['regions'][0]['region'], 'date': z['date'], 'time': z['time'], 'currency': z['currency'], 'os':x["os"], 'type':x['type'], 'price': x['price'], 'utilization': x['utilization']})
 
-@app.route('/ec2-api/api/v1.0/tasks', methods=['POST'])
-@auth.login_required
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
+   return jsonify({'result': data_array})
 
-@app.route('/ec2-api/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+@app.route('/api/v1.0/get_region/<region>', methods=['GET'])
 @auth.login_required
-def update_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    if not request.json:
-        abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode:
-        abort(400)
-    if 'description' in request.json and type(request.json['description']) is not unicode:
-        abort(400)
-    if 'done' in request.json and type(request.json['done']) is not bool:
-        abort(400)
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': task[0]})
+def get_region(region):
+    region_array=[]
+    
+    t= collection.find()
 
-@app.route('/ec2-api/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+    for z in t:
+        for h in z:
+            if h == "regions":
+                for x in z[h][0]['instanceTypes']:
+                    region_array.append({'region':region, 'date': z['date'], 'time': z['time'], 'currency': z['currency'], 'os':x["os"], 'type':x['type'], 'price': x['price'], 'utilization': x['utilization']})
+
+    return jsonify({'result': region_array})
+
+@app.route('/api/v1.0/get_family/<family>', methods=['GET'])
 @auth.login_required
-def delete_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    tasks.remove(task[0])
-    return jsonify({'result': True})
+def get_family(family):
+    family_array=[]
+
+    t= collection.find()
+    for x in t:
+        for z in x['regions']:
+            for n in z:
+                if n == 'instanceTypes':
+                    for j in z[n]:                    
+                        if j['type'] == family:
+                            family_array.append(j)
+    return jsonify({'result': family_array})
+
+@app.route('/api/v1.0/get_os/<os>', methods=['GET'])
+@auth.login_required
+def get_os(os):
+    os_array=[]
+
+    t= collection.find()
+    for x in t:
+        for z in x['regions']:
+            for n in z:
+                if n == 'instanceTypes':
+                    for j in z[n]:                       
+                        if j['os'] == os:
+                            os_array.append(j)
+    return jsonify({'result': os_array})
+
+@app.route('/api/v1.0/query')
+@auth.login_required
+def run():
+	parser = reqparse.RequestParser()
+	parser.add_argument('date', help= 'Invalid Date')
+	parser.add_argument('region', help = 'Invalid Region')
+	parser.add_argument('type',help = 'Invalid Type')
+	args = parser.parse_args()
+
+	if args['date'] ==None :
+		return jsonify(date=args['date'],region = args['region'],type = args['type'])
+	else:
+		if args['type']==None and args['region'] == None:
+			return json_util.dumps(collection.find({'date':args['date']}))
+		elif args['type'] ==None:
+			return json_util.dumps(collection.find({'date':args['date'],'regions.region':args['region']}))
+		else:
+			x = collection.find_one({'date':args['date'],'regions.region':args['region']})
+			if x!=None and x['regions']!=None and x['regions'][0]!=None:
+				for instance in x['regions'][0]['instanceTypes']:
+					if instance['type']==args['type']:
+						print(instance['price'])
+						return json_util.dumps(instance)
+				return json_util.dumps([])
+			else:
+				return json_util.dumps([])
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not Found'}), 404)
+
+@app.errorhandler(500)
+def server_error(error):
+    return make_response(jsonify({'error': 'Internal Server Error'}), 500)
 
 @app.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 app.run(host= '0.0.0.0')
-
+	
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True, port=80)
 
