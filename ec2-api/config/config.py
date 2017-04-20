b0e000b275ae472c0f3d917c1f44ee31e019ebe2
Chathuranga Bandara Abeyrathna <chaturanga50@gmail.com>
2017-04-20 19:56:29 +0530

    added config file

---
+++ ec2-api/config/config.py
@@ -0,0 +1,20 @@
+#mongo connection details
+MONGO_IP='ec2-52-33-101-11.us-west-2.compute.amazonaws.com'
+MONGO_PORT=27017
+MONGO_DB='spot'
+MONGO_COLLECTION='spotinstance'
+MONGO_USER=''
+MONGO_PASSWORD=''
+
+#mongo mlab connection details
+M_MONGO_IP='ds153179.mlab.com'
+M_MONGO_PORT=53179
+M_MONGO_DB='ec2-api'
+M_MONGO_COLLECTION='data'
+M_MONGO_USER='test'
+M_MONGO_PASSWORD='123456'
+
+#redis connection details
+REDIS_IP='127.0.0.1'
+REDIS_PORT=6379
+REDIS_PASSWORD='kiFYmWZB8F'
